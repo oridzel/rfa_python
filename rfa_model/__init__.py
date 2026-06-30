87c1a7e60c8412436e325293149daa5980f96802
onr5 <olga.ridzel@nist.gov>
2026-06-30 14:52:04 -0400

    init.py updated

--- rfa_model/__init__.py
+++ rfa_model/__init__.py
@@ -5,3 +5,33 @@ Python model for RFA electron trajectory, emission, cascade, and current account
 """
 
 __version__ = "0.1.0"
+
+from .io import load_field_npz, save_field_npz
+
+from .fields import (
+    attach_default_owner_name_map,
+    build_field_interpolators,
+    build_potential_interpolator,
+)
+
+from .geometry import (
+    default_sample_parts,
+    load_and_align_sample_assembly,
+    load_and_align_grid_frames,
+    sample_bounds,
+)
+
+from .samplers import (
+    load_default_surface_models,
+)
+
+from .cascade import (
+    run_cascade_batch_parallel,
+    print_cascade_batch_summary,
+    save_cascade_batch_tables,
+)
+
+from .accounting import (
+    summarize_cascade_accounting,
+    add_per_primary_to_current_counts,
+)
